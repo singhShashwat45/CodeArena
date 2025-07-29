0fbe4345bdd646f3f740099fd7c1b16364a53919
Shashwat Singh <shashwat0045@gmail.com>
2025-07-30 01:03:20 +0530

    Added token expiry in login.js

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -7,19 +7,21 @@ const secretToken = process.env.SECRET_TOKEN;
 router.post('/', async (req, res) => {
   try {
     const { username, password } = req.body;
-
+    if (!username || !password) {
+      return res.status(400).json({ msg: "Username and password are required." });
+    }
     const users = await db.collection('users').find({}).toArray();
     const admins = await db.collection('admins').find({}).toArray();
 
     const foundAdmin = admins.find(admin => admin.username === username);
     if (foundAdmin && foundAdmin.password === password) {
-      const token = jwt.sign({ id: username, access: "admin" }, secretToken);
+      const token = jwt.sign({ id: username, access: "admin" }, secretToken, { expiresIn: '7h' });
       return res.status(200).json({ token, access: "admin" });
     }
 
     const foundUser = users.find(user => user.username === username);
     if (foundUser && foundUser.password === password) {
-      const token = jwt.sign({ id: username, access: "user" }, secretToken);
+      const token = jwt.sign({ id: username, access: "user" }, secretToken, { expiresIn: '7h' });
       return res.status(200).json({ token, access: "user" });
     }
 
